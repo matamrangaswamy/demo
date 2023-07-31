ef6348aca31962f8da310f2bbc213087d7a63f90
matamrangaswamy <matamranga1997@gmail.com>
2023-07-31 19:05:44 +0530

    Find consolants / Vowels

---
+++ VowelsOrConsolants.java
@@ -0,0 +1,10 @@
+public class VowelsOrConsolants {
+	public static void main(String[] args) {
+		char ch = 't';
+		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I'
+				|| ch == 'O' || ch == 'U') {
+			System.out.println(ch + " Is a vowels");
+		} else
+			System.out.println(ch + " Is a consolants");
+	}
+}
