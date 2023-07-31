59116c71dd35a4d12bb02fba87808b5b62dca798
matamrangaswamy <matamranga1997@gmail.com>
2023-07-31 18:53:01 +0530

    Added the logic

---
+++ BiggestValue.java
@@ -0,0 +1,15 @@
+package Numbers;
+
+public class BiggestValue {
+public static void main(String[] args) {
+	int a=6;
+	int b=2;
+	int c=3;
+	int big=a;
+	if(big<b)
+		big=b;
+	if(big<c)
+		big=c;
+	System.out.println(big);
+}
+}
